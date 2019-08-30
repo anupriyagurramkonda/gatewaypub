de093aa14e40361a603e46eabb97d9a94c40db65
anupriyagurramkonda <50578832+anupriyagurramkonda@users.noreply.github.com>
2019-08-30 09:56:53 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo "Region:" $1
-echo "Bucket Name:" $2
+echo 'Region:'$region 
+echo 'vpc_cidr:'$VPC_CIDR
+echo 'env:'$ENV
+echo 'PublicSubnet:'$PublicSubnetAZCIDR
 
 ./terraform init
 
-./terraform apply -auto-approve -var 'region='$1 
+./terraform apply -auto-approve -var 'region='$region  -var 'aws_access_key='$AWS_ACCESS_KEY -var 'aws_secret_key='$AWS_SECRET_KEY -var 'vpc_cidr='$VPC_CIDR -var 'env='ENV -var 'subnet_cidr='$PublicSubnetAZCIDR    
 
 
 
