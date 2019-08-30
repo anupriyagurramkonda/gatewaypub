f7495cbfe3f3d8bd97aae431cd78f99e3f49191c
anupriyagurramkonda <50578832+anupriyagurramkonda@users.noreply.github.com>
2019-08-30 10:10:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,60 +5,27 @@ resource "aws_vpc" "VPC" {
     enable_dns_support = "true"
     enable_dns_hostnames = "true"
     enable_classiclink = "false"
-    tags {
+    tags = {
         Name = "${var.ENV}-VPC"
     }
 }
 
 # Subnets
-resource "aws_subnet" "Public-AZ1-SN" {
+resource "aws_subnet" "Public-AZ-SN" {
     vpc_id = "${aws_vpc.VPC.id}"
-    cidr_block = "${var.PublicSubnetAZ1CIDR}"
+    cidr_block = "${var.PublicSubnetAZCIDR}"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-central-1a"
+    availability_zone = "us-east-1a"
 
-    tags {
-        Name = "${var.ENV}-VPC-Public-AZ1-SN"
+    tags = {
+        Name = "${var.ENV}-VPC-Public-AZ-SN"
     }
 }
-
-resource "aws_subnet" "Public-AZ2-SN" {
-    vpc_id = "${aws_vpc.VPC.id}"
-    cidr_block = "${var.PublicSubnetAZ2CIDR}"
-    map_public_ip_on_launch = "true"
-    availability_zone = "eu-central-1b"
-
-    tags {
-        Name = "${var.ENV}-VPC-Public-AZ2-SN"
-    }
-}
-
-resource "aws_subnet" "Private-AZ1-SN" {
-    vpc_id = "${aws_vpc.VPC.id}"
-    cidr_block = "${var.PrivateSubnetAZ1CIDR}"
-    map_public_ip_on_launch = "false"
-    availability_zone = "eu-central-1a"
-
-    tags {
-        Name = "${var.ENV}-VPC-Private-AZ1-SN"
-    }
-}
-resource "aws_subnet" "Private-AZ2-SN" {
-    vpc_id = "${aws_vpc.VPC.id}"
-    cidr_block = "${var.PrivateSubnetAZ2CIDR}"
-    map_public_ip_on_launch = "false"
-    availability_zone = "eu-central-1b"
-
-    tags {
-        Name = "${var.ENV}-VPC-Private-AZ2-SN"
-    }
-}
-
 # Internet GW
 resource "aws_internet_gateway" "Igw" {
     vpc_id = "${aws_vpc.VPC.id}"
 
-    tags {
+    tags = {
         Name = "${var.ENV}-VPC-IGW"
     }
 }
@@ -71,7 +38,7 @@ resource "aws_eip" "nat" {
 #Nat_gw
 resource "aws_nat_gateway" "Ngw" {
   allocation_id = "${aws_eip.nat.id}"
-  subnet_id     = "${aws_subnet.Public-AZ1-SN.id}"
+  subnet_id     = "${aws_subnet.Public-AZ-SN.id}"
 
   tags = {
      Name = "${var.ENV}-VPC-NGW"
