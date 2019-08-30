cbe47923575420286d0bce2c0e61ce520e793d0b
anupriyagurramkonda <50578832+anupriyagurramkonda@users.noreply.github.com>
2019-08-30 10:58:37 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 echo 'Region:'$region 
 echo 'vpc_cidr:'$vpc_cidr
-echo 'name:'$Name
+echo 'name:'$name
 echo 'subnet:'$subnet_cidr
 echo 'subnet_az:'$subnet_az
 
 ./terraform init
 
-./terraform apply -auto-approve -var 'region='$region  -var 'aws_access_key='$AWS_ACCESS_KEY -var 'aws_secret_key='$AWS_SECRET_KEY -var 'vpc_cidr='$vpc_cidr -var 'name='$Name -var 'subnet_cidr='$subnet_cidr -var 'subnet_az='$subnet_az    
+./terraform apply -auto-approve -var 'region='$region  -var 'aws_access_key='$AWS_ACCESS_KEY -var 'aws_secret_key='$AWS_SECRET_KEY -var 'vpc_cidr='$vpc_cidr -var 'name='$name -var 'subnet_cidr='$subnet_cidr -var 'subnet_az='$subnet_az    
 
 
 
