af6f3558c0ea099d2904e3a539e2e9c7a7e7c79b
anupriyagurramkonda <50578832+anupriyagurramkonda@users.noreply.github.com>
2019-08-30 09:44:38 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,44 @@
+variable "region" {
+	
+}
+
+variable "aws_access_key" {
+
+}
+
+variable "aws_secret_key" {
+
+}
+
+variable "vpc_cidr" {
+	
+}
+
+variable "vpc_name" {
+	
+}
+
+variable "subnets_cidr1" {
+
+}
+
+variable "azs1" {
+	
+}
+
+variable "azs2" {
+        
+}
+
+variable "subnets_cidr2" {
+        
+}
+
+variable "subnet_pub" {
+	
+}
+
+variable "subnet_pri" {
+	
+}
+
