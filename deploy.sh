b30fa6772b6b564b679bb8d967e038e86171725c
anupriyagurramkonda <50578832+anupriyagurramkonda@users.noreply.github.com>
2019-08-30 10:00:08 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ echo 'PublicSubnet:'$PublicSubnetAZCIDR
 
 ./terraform init
 
-./terraform apply -auto-approve -var 'region='$region  -var 'aws_access_key='$AWS_ACCESS_KEY -var 'aws_secret_key='$AWS_SECRET_KEY -var 'vpc_cidr='$VPC_CIDR -var 'env='ENV -var 'subnet_cidr='$PublicSubnetAZCIDR    
+./terraform apply -auto-approve -var 'region='$region  -var 'aws_access_key='$AWS_ACCESS_KEY -var 'aws_secret_key='$AWS_SECRET_KEY -var 'vpc_cidr='$VPC_CIDR -var 'env='$ENV -var 'subnet_cidr='$PublicSubnetAZCIDR    
 
 
 
